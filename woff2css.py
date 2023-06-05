56d59ff517447b9d19b499c9f21ec11ab91a3d18
Albert Calasanz <acalasanzs@gmail.com>
2023-06-05 20:02:47 +0200

    AA

---
+++ woff2css.py
@@ -0,0 +1,22 @@
+import os
+from os import listdir,getcwd
+from os.path import isfile, join, abspath, splitext
+candidates = [
+    ".woff2",
+    ".woff"
+]
+def main(path):
+    with open("font.css", "w") as css:
+        for f in listdir(path):
+            file = join(path,f)
+            ext = splitext(file)[1]
+            print(ext)
+            if isfile(file) and ext in candidates: 
+                css.write("@font-face")
+                css.write("{")
+                css.write(f"\n\tfont-family: '{f.replace('-',' ')}';")
+                css.write(f"\n\tsrc: url('{f}');")
+                css.write("\n}\n")
+if __name__ == '__main__':
+    current_directory = abspath(getcwd())
+    main(current_directory)
