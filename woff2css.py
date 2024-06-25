167ae128a8fd7cc589132be8e0351bca5f7ee9da
Albert Calasanz <acalasanzs@gmail.com>
2024-06-25 19:46:32 +0200

    little bug

--- woff2css.py
+++ woff2css.py
@@ -20,10 +20,10 @@ def main(path):
                 css.write("\n}\n")
 if __name__ == '__main__':
     current_directory = abspath(getcwd())
+    current_directory = os.path.abspath(os.getcwd())
     parser = argparse.ArgumentParser(description="AnyFont v1")
     parser.add_argument("-d", type=str, help="directory to search")
     args = parser.parse_args()
-    current_directory = os.path.abspath(os.getcwd())
     if bool(args.d):
         current_directory = args.d
     main(current_directory)
